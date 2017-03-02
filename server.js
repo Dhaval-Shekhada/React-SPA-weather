00d2fcee72a488bb08d77f2eabd9c1c43300daa5
Dhaval_shekhada <dhavalshekhada@gmail.com>
2017-03-02 14:24:24 +0100

    heroku set

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 var express = require('express');
 const cors = require('cors');
-
+const PORT = process.env.PORT || 3000;
 var app = express();
+app.use(function(req,res,next){
 
+  if(req.header['X-forwarded-porto'] === 'http'){
+    next();
+  }else{
+    res.redirect('http://'+ req.hostname +req.url);
+  }
+
+});
 app.use(express.static('public'));
 
-app.listen(3000,function(){
-  console.log("server is on 3000");
+app.listen(PORT,function(){
+  console.log("server is on "+ PORT);
 });
